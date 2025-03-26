7c1d269fe1beb2a88459682f66c9f8a1a46d4ad0
MattheDev53 <110939089+MattheDev53@users.noreply.github.com>
2025-03-26 07:14:21 -0500

    Set up base user

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "6744 Developer Environments";
+
+  inputs = {
+    # Don't know what I'll do for this yet,
+    # this'll be running under WSL most likely,
+    # but I'm not sure if I'll run NixOS WSL
+    # or Ubuntu WSL with Nix
+    nixpkgs.url = ""; 
+    home-manager.url = "github:nix-community/home-manager"
+  };
+  outputs = { self, nixpkgs, home-manager }:{
+    homeConfigurations = {
+      user = home-manager.lib.homeManagerConfiguration {
+        pkgs = import nixpkgs
+          { system = "x86_64-linux"; };
+        modules = [ ./users/user/default.nix ];
+      };
+    };
+  };
+}
---
+++ users/user/user.nix
@@ -0,0 +1,7 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "user";
+  home.homeDirectory = "/home/user/";
+  home.stateVersion = "24.11";
+}
