9523268d96381f0d61766189537f815bd79dc6ff
MattheDev53 <110939089+MattheDev53@users.noreply.github.com>
2025-03-27 17:20:39 -0500

    Fix VSCode stuff in the flake

--- users/magames/programs/vscode.nix
+++ users/magames/programs/vscode.nix
@@ -23,9 +23,38 @@ programs.vscode = {
         redhat.java
       ];
       userSettings = {
-        "editor.fontFamily" = "JetBrains Mono";
-        "editor.fontLigatures" = true;
+        "extensions.autoUpdate" = false;
+        "extensions.autoCheckUpdates" = false;
+        "extensions.ignoreRecommendations" = true;
+        "update.mode" = "none";
+        "update.showReleaseNotes" = false;
+        "http.systemCertificates" = false;
+        "git.autofetch" = true;
+        "java.completion.favoriteStaticMembers" = [
+          "org.junit.Assert.*"
+          "org.junit.Assume.*"
+          "org.junit.jupiter.api.Assertions.*"
+          "org.junit.jupiter.api.Assumptions.*"
+          "org.junit.jupiter.api.DynamicContainer.*"
+          "org.junit.jupiter.api.DynamicTest.*"
+          "org.mockito.Mockito.*"
+          "org.mockito.ArgumentMatchers.*"
+          "org.mockito.Answers.*"
+        ];
+        "files.autoSave" = "afterDelay";
+        "java.completion.matchCase" = "off";
+        "git.ignoreMissingGitWarning" = true;
         "workbench.colorTheme" = "Gruvbox Material Dark";
+        "settingsSync.ignoredSettings" = [];
+        "editor.fontFamily" = "JetBrains Mono";
+        "workbench.iconTheme" = "gruvbox-material-icon-theme";
+        "redhat.telemetry.enabled" = false;
+        "chat.commandCenter.enabled" = false;
+        "editor.fontLigatures" = true;
+        "git.confirmSync" = false;
+        "diffEditor.ignoreTrimWhitespace" = false;
+        "wpilib.autoStartRioLog" = false;
+        "editor.tabSize" = 2;
       };
     };
   };
--- users/user/programs/vscode.nix
+++ users/user/programs/vscode.nix
@@ -11,12 +11,50 @@ programs.vscode = {
         arrterian.nix-env-selector
   
         vscjava.vscode-java-pack
+        
+        vscjava.vscode-java-debug
+        vscjava.vscode-java-dependency
+        vscjava.vscode-java-test
+        vscjava.vscode-gradle
+        vscjava.vscode-maven
+        redhat.java
+
       ];
       userSettings = {
-        "editor.fontFamily" = "JetBrains Mono";
-        "editor.fontLigatures" = true;
+        "extensions.autoUpdate" = false;
+        "extensions.autoCheckUpdates" = false;
+        "extensions.ignoreRecommendations" = true;
+        "update.mode" = "none";
+        "update.showReleaseNotes" = false;
+        "http.systemCertificates" = false;
+        "git.autofetch" = true;
+        "java.completion.favoriteStaticMembers" = [
+          "org.junit.Assert.*"
+          "org.junit.Assume.*"
+          "org.junit.jupiter.api.Assertions.*"
+          "org.junit.jupiter.api.Assumptions.*"
+          "org.junit.jupiter.api.DynamicContainer.*"
+          "org.junit.jupiter.api.DynamicTest.*"
+          "org.mockito.Mockito.*"
+          "org.mockito.ArgumentMatchers.*"
+          "org.mockito.Answers.*"
+        ];
+        "files.autoSave" = "afterDelay";
+        "java.completion.matchCase" = "off";
+        "git.ignoreMissingGitWarning" = true;
         "workbench.colorTheme" = "Gruvbox Material Dark";
+        "settingsSync.ignoredSettings" = [];
+        "editor.fontFamily" = "JetBrains Mono";
+        "workbench.iconTheme" = "gruvbox-material-icon-theme";
+        "redhat.telemetry.enabled" = false;
+        "chat.commandCenter.enabled" = false;
+        "editor.fontLigatures" = true;
+        "git.confirmSync" = false;
+        "diffEditor.ignoreTrimWhitespace" = false;
+        "wpilib.autoStartRioLog" = false;
+        "editor.tabSize" = 2;
       };
+
     };
   };
 }
