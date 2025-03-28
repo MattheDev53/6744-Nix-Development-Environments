2fcfee87c8b46f70d8da157cb018b73b4b917626
MattheDev53 <110939089+MattheDev53@users.noreply.github.com>
2025-03-28 14:31:25 -0500

    Shell script to easily install HM Flake

---
+++ getFlake.sh
@@ -0,0 +1,14 @@
+#!/bin/env bash
+
+# Install Nix if not installed already
+if [ "$(which nix)" == "" ]; then
+  sh <(curl -L https://nixos.org/nix/install) --daemon
+fi
+
+# Checks if Flakes and Nix Command are enabled
+if [ "$(grep 'experimental-features = nix-command flakes' /etc/nix/nix.conf)" == "" ]; then
+  echo "experimental-features = nix-command flakes" | sudo tee /etc/nix/nix.conf
+fi
+
+# Updates/Installs the flake from GitHub
+nix run github:nix-community/home-manager switch -- --flake github:MattheDev53/6744-Nix-Development-Environments
