720b9b8d4728c7dbd74128696fe6cb3ab8925297
MattheDev53 <110939089+MattheDev53@users.noreply.github.com>
2025-04-01 09:37:06 -0500

    minor spelling mistake

--- getFlake.sh
+++ getFlake.sh
@@ -2,7 +2,7 @@
 
 # Updates/Installs the flake from GitHub
 if [ "$(which nix)" == "" ]; then
-  echo "Could not find Nix. If you have already installed Nix, Try running 'exec \$SHELL', then try again"
+  echo "Could not find Nix. If you have already installed Nix, try running 'exec \$SHELL', then try again"
 else
   nix run github:nix-community/home-manager switch -- --flake github:MattheDev53/6744-Nix-Development-Environments
 fi
